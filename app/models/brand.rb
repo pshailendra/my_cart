8b46fc7a32eba3a4c2fd78b6b422bcd81b70eadd
Shailendra <shailedrammec@gmail.com>
2015-01-24 00:17:11 +0530

    Image Validation Added

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,5 @@
 class Brand < ActiveRecord::Base
+
+  has_attached_file :image, styles: {:medium => '150x260>', :thumb => '40x65>'}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
